01179f77e4fea9adeedd3386759eabf44814609f
sieva-cmd <musyokasieva@gmail.com>
2022-01-27 14:03:40 +0300

    updating changes

--- appointment/script.js
+++ appointment/script.js
@@ -1,5 +1,5 @@
-  
-  function Booking(name, email, phoneNo, doctor, message) {
+
+function Booking(name, email, phoneNo, doctor, message) {
   this.name = name;
   this.email = email;
   this.phoneNo = phoneNo;
@@ -7,11 +7,11 @@
   this.message = message;
 }
 $(document).ready(function () {
-$("#btn").click(function (event) {
-$("#form").show();
-$(".check").show();
-$("#btn").hide();
-$(".show").hide();
+  $("#btn").click(function (event) {
+    $("#form").show();
+    $(".check").show();
+    $("#btn").hide();
+    $(".show").hide();
   })
   $("#form").submit(function (event) {
     event.preventDefault()
@@ -20,17 +20,18 @@ $(".show").hide();
     var myPhoneNo = $("#number").val();
     var myServices = $("#service").val();
     var myDoctor = $("#doctors").val();
-var myBooking = new Booking( myName, myEmail, myEmail, myPhoneNo, myDoctor, myServices)
-//alert("thankyou we have recevied")
-$("#p").show();
-$("#patient").html(myName);
-$("#service").html(myServices);
-$("#doc").html(myDoctor);
-$("#form").hide();
-$("#btn").show();
-$("#check").hide();
+    var myBooking = new Booking(myName, myEmail, myEmail, myPhoneNo, myDoctor, myServices)
+    //alert("thankyou we have recevied")
+    $("#p").show();
+    $("#patient").html(myName);
+    $("#service").html(myServices);
+    $("#doc").html(myDoctor);
+    $("#form").hide();
+    $("#btn").show();
+    $("#check").hide();
   })
 
- })    
+
+})
 
 
